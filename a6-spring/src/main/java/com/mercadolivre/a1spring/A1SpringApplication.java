3aef8f09e861a464eac5bab766f1dc32ca14cc54
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-03-17 16:14:49 -0300

    added submodule projeto em grupo

---
+++ a6-spring/src/main/java/com/mercadolivre/a1spring/A1SpringApplication.java
@@ -0,0 +1,15 @@
+package com.mercadolivre.a1spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A1SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A1SpringApplication.class, args);
+
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ a6-spring/src/test/java/com/mercadolivre/a1spring/A1SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.a1spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A1SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
