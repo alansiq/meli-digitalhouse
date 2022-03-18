ba011aeebf36916aacb4b635ea63786f8cd65c48
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-03-18 12:38:40 -0300

    Added a9-spring-pratica1

---
+++ a9-spring-pratica1/src/main/java/com/mercadolivre/a9springpratica1/A9SpringPratica1Application.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.a9springpratica1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A9SpringPratica1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A9SpringPratica1Application.class, args);
+	}
+
+}
---
+++ a9-spring-pratica1/src/test/java/com/mercadolivre/a9springpratica1/A9SpringPratica1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.a9springpratica1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A9SpringPratica1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
